b7bea26380e9ffbccfe8990b051f9a539fd37475
Wasif Hyder <wasif_hyder@hotmail.com>
2016-04-13 12:22:34 +0800

    Fixing speed issue

--- app/app.py
+++ app/app.py
@@ -40,6 +40,6 @@ def index():
 if __name__ == '__main__':
     import platform
     if platform.dist('Ubuntu'):
-        app.run(host='0.0.0.0', port=80)
+        app.run(host='0.0.0.0', port=80, threaded=True)
     else:
         app.run(debug=True, port=81)
